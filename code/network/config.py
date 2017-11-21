81b2a6248bdbf5153d302df7d465446b82625cd4
Arjun Karpur <arjun.karpur@gmail.com>
2017-11-21 15:00:32 -0600

    Create base network files

---
+++ code/network/config.py
@@ -0,0 +1,8 @@
+
+# Filepaths
+DATA_DIR = "../../data/shapenet/chair/V1"
+DATA_LABELS_FP = "../../data/shapenet/chair/V1/annots.txt"
+#TODO
+
+# Learning parameters
+#TODO
---
+++ code/network/train.py
@@ -0,0 +1,9 @@
+import torch
+import config
+
+def main():
+  print config.DATA_DIR
+  print config.DATA_LABELS_FP
+
+if __name__ == "__main__":
+    main()
