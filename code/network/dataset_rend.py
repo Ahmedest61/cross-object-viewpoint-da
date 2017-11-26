e75e8e391b1b04f6f91dafc2c7f52abbb811f14b
Arjun Karpur <arjun.karpur@gmail.com>
2017-11-26 16:25:04 -0600

    Implemented rendered dataset

--- code/network/dataset_rend.py
+++ code/network/dataset_rend.py
@@ -1,17 +1,46 @@
+import os
+import cv2
 import torch
-from torch.utils.data import Dataset, Dataloader
+from torch.utils.data import Dataset, DataLoader
 
 class RenderedDataset(Dataset):
-  def __init__(self, data_dir, data_labels_fp):
-    #TODO
-    pass
+
+  """ 
+    Instance vars
+    - self.data_dir => string (dir holding all ims)
+    - self.ims_list => list
+    - self.images => dictionary (key=filename, val=annot)
+    - self.transform => transformation on image
+  """
+
+  def __init__(self, data_dir, data_labels_fp, transform=None):
+    
+    # Get list of all PNGs in data_dir
+    self.data_dir = data_dir
+    self.ims_list = [f for f in os.listdir(data_dir) if (os.path.isfile(os.path.join(data_dir, f)) and f.split(".")[-1] == "png")]
+
+    # Load labels
+    data_labels_f = open(data_labels_fp, 'r')
+    lines = data_labels_f.readlines()
+    data_labels_f.close()
+    self.images = {}
+    for l in lines:
+      split = l.strip().split(",")
+      im_name = "%s.png" % split[0]
+      if im_name in self.ims_list:
+        self.images[im_name] = [int(split[1]), int(split[2])]
+
+    # Save transform
+    self.transform = transform
 
   def __len__(self):
-    #TODO
-    pass
+    return len(self.ims_list)
 
   def __getitem__(self, idx):
-    #TODO
-    pass
-
-
+    im_name = self.ims_list[idx]
+    im_fp = os.path.join(self.data_dir, im_name)
+    image = cv2.imread(im_fp)
+    sample = {"image": image, "annot": self.images[im_name]}
+    if self.transform:
+        sample = self.transform(sample)
+    return sample
--- code/network/train.py
+++ code/network/train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import config
 from dataset_rend import RenderedDataset
@@ -5,8 +6,9 @@ from dataset_rend import RenderedDataset
 def main():
 
   # Create training DataLoader
+  train_ims_dir = os.path.join(config.DATA_DIR, 'train')
   train_dataset = \
-    RenderedDataset(config.DATA_DIR, config.DATA_LABELS_FP)
+    RenderedDataset(train_ims_dir, config.DATA_LABELS_FP)
 
 if __name__ == "__main__":
     main()
