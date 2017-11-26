af0b299c352f4223f4c83e65e44c03db217ae805
Arjun Karpur <arjun.karpur@gmail.com>
2017-11-26 16:37:16 -0600

    Debug printing

--- code/network/config.py
+++ code/network/config.py
@@ -1,4 +1,3 @@
-
 # Filepaths
 DATA_DIR = "../../data/shapenet/chair/V1"
 DATA_LABELS_FP = "../../data/shapenet/chair/V1/annots.txt"
@@ -7,7 +6,23 @@ DATA_LABELS_FP = "../../data/shapenet/chair/V1/annots.txt"
 # Running parameters
 GPU = True
 MULTI_GPU = True
-TEST_AFTER = True
+TEST_AFTER_TRAIN = True
 
 # Learning parameters
 #TODO
+
+# Debugging print method
+def PRINT_CONFIG():
+  print "~~~~~ BEGIN CONFIG ~~~~~"
+  print " "
+
+  print "DATA_DIR:\t", DATA_DIR
+  print "DATA_LABELS_FP:\t", DATA_LABELS_FP
+  print " "
+
+  print "GPU:\t", GPU
+  print "MULTI_GPU:\t", MULTI_GPU
+  print "TEST_AFTER_TRAIN:\t", TEST_AFTER_TRAIN
+
+  print "~~~~~~ END CONFIG ~~~~~~"
+  pass
--- code/network/train.py
+++ code/network/train.py
@@ -1,9 +1,15 @@
 import os
 import torch
 import config
+import datetime
 from dataset_rend import RenderedDataset
 
 def main():
+
+  # Print beginning debug info
+  print "Beginning training..."
+  print datetime.datetime.now()
+  config.PRINT_CONFIG()
 
   # Create training DataLoader
   train_ims_dir = os.path.join(config.DATA_DIR, 'train')
