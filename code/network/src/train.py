7db8d24b43b3e6914ca4dfb23a77cd210d398d25
Arjun Karpur <arjun.karpur@gmail.com>
2017-11-27 16:18:52 -0600

    Add test set testing to train.py

--- code/network/src/train.py
+++ code/network/src/train.py
@@ -119,9 +119,33 @@ def train_model(model, train_dataloader, val_dataloader, loss_f, optimizer, expl
 def save_model_weights(model, filepath):
   torch.save(model.state_dict(), filepath)
 
-def test_model(model, test_dataloader):
-  #TODO
-  pass
+def test_model(model, test_dataloader, loss_f):
+  test_loss = test_correct = 0
+  print_interval = 100
+  batch_count = 0
+  for data in test_dataloader:
+    inputs, annots = data['image'], data['annot']
+
+    # Wrap as pytorch autograd Variable
+    if config.GPU and torch.cuda.is_available():
+      inputs = Variable(inputs.cuda())
+      annots = Variable(annots.cuda())
+    else:
+      inputs = Variable(inputs)
+      annots= Variable(annots)
+
+    # Forward pass and calculate loss
+    outputs = model(inputs)
+    _, preds = torch.max(outputs.data, 1)
+    loss = loss_f(outputs, annots)
+    test_loss += loss.data[0]
+    test_correct += torch.sum(preds == annots.data)
+
+  # Report epoch results
+  num_images = len(test_dataloader.dataset)
+  test_loss = float(test_loss) / float(num_images)
+  test_acc = float(test_correct) / float(num_images)
+  log_print("[TEST SET] %i ims - Loss: %f   Acc: %f" % (num_images, test_loss, test_acc))
 
 #####################
 #    END HELPERS    #
@@ -187,7 +211,7 @@ def main():
 
   # Test and report accuracy
   log_print("Testing model on test set...")
-  accuracy = test_model(model, test_dataloader)
+  accuracy = test_model(model, test_dataloader, loss_f)
 
   log_print("Script DONE!")
 
